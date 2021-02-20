3611092d3dcf76d1f9f3b61a96d577b59805db6a
BilbaoJ <jessicabilbao02@gmail.com>
2021-02-20 06:55:05 -0500

    Ejercicio

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -3,7 +3,6 @@ function fibonacci(n) {
     let a = 1;
     let b = 1;
     let c = 1;
-    //console.log(`${a}, ${b}`);
     for (let i = 1; i < n; i++) {
         console.log(`${c}`);
         a = b;
